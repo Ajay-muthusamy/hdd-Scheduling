bfa76d56a74bfe7a0a2f2e83cec631303e40f410
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-06-14 07:42:39 +0530

    commit

---
+++ hdd/server.js
@@ -0,0 +1,15 @@
+const express =require('express');
+const app = express();
+const path = require('path');
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(8585, () => {
+    console.log('Server is running on http://localhost:8585');
+});
